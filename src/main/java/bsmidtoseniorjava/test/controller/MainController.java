d9cd222cab2e271bde8045d0f6ba24a9d66a0dcf
Marton Szekely <marton.szekely@blackswan.com>
2020-09-15 11:30:00 +0200

    dev: added impl. for delete and update user

--- src/main/java/bsmidtoseniorjava/test/controller/MainController.java
+++ src/main/java/bsmidtoseniorjava/test/controller/MainController.java
@@ -3,10 +3,10 @@ package bsmidtoseniorjava.test.controller;
 import bsmidtoseniorjava.test.model.User;
 import bsmidtoseniorjava.test.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RequestMapping("api/user")
 @RestController
@@ -22,5 +22,26 @@ public class MainController {
     @PostMapping
     public void addUser(@RequestBody User user) {
         userService.addUser(user);
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping(path = "{id}")
+    public User getUserById(@PathVariable("id") UUID id) {
+        //TODO: implement catch part in case of invalid/ not found ID
+        return userService.getUserById(id).orElse(null);
+    }
+
+    @DeleteMapping(path = "{id}")
+    public void deleteUserById(@PathVariable("id") UUID id) {
+        userService.deleteUser(id);
+    }
+
+    @PutMapping(path = "{id}")
+    public void updateUser(@PathVariable("id") UUID id, @RequestBody User userToUpdate) {
+        userService.updateUser(id,userToUpdate);
     }
 }
