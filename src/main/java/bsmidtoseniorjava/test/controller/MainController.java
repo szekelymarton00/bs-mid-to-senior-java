b3f549ed7b95a23a035c65908340502331b10199
Marton Szekely <marton.szekely@blackswan.com>
2020-09-18 07:54:58 +0200

    dev: added logs in MainController

--- src/main/java/bsmidtoseniorjava/test/controller/MainController.java
+++ src/main/java/bsmidtoseniorjava/test/controller/MainController.java
@@ -2,15 +2,24 @@ package bsmidtoseniorjava.test.controller;
 
 import bsmidtoseniorjava.test.model.User;
 import bsmidtoseniorjava.test.services.UserService;
+import org.apache.logging.log4j.LogManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.UUID;
+import org.apache.logging.log4j.Logger;
 
 @RestController
 @RequestMapping("api/user")
 public class MainController {
 
     private final UserService userService;
+
+    private static final Logger log;
+
+    static {
+        log = LogManager.getLogger(MainController.class);
+    }
 
     @Autowired
     public MainController(UserService userService) {
@@ -19,28 +28,33 @@ public class MainController {
 
     @PostMapping
     public void addUser(@RequestBody User user) {
+        log.debug("Adding user: " + user.getUsername() +" to db");
         userService.addUser(user);
     }
 
     @GetMapping
     public Iterable<User> getAllUsers() {
+        log.debug("Retrieving all users in db");
         return userService.getAllUsers();
     }
 
     @GetMapping(path = "{id}")
     public User getUserById(@PathVariable("id") UUID id) {
         //TODO: implement catch part in case of invalid/ not found ID
+        log.debug("Retrieving user with ID: " + id);
         return userService.getUserById(id).orElse(null);
     }
 
     @DeleteMapping(path = "{id}")
     public void deleteUserById(@PathVariable("id") UUID id) {
+        log.debug("Deleting user with ID: " + id);
         userService.deleteUser(id);
     }
 
     @PutMapping(path = "{id}")
     public void updateUser(@PathVariable("id") UUID id, @RequestBody User userToUpdate) {
         if (userService.getUserById(id).isPresent()) {
+            log.debug("Updating user with ID: " + id);
             userService.updateUser(id,userToUpdate);
         }
     }
