18b91620ad270821fe646735b9bb6c5cfa140018
Marton Szekely <marton.szekely@blackswan.com>
2020-09-18 14:54:36 +0200

    dev: WIP added task table

--- src/main/resources/db/migration/V1__UserTable.sql
+++ src/main/resources/db/migration/V1__UserTable.sql
@@ -1,5 +1,5 @@
 CREATE TABLE bsuser (
-    id UUID NOT NULL PRIMARY KEY,
+    user_id UUID NOT NULL PRIMARY KEY,
     username VARCHAR(99) NOT NULL,
     first_name VARCHAR(99),
     last_name VARCHAR(99),
@@ -7,8 +7,8 @@ CREATE TABLE bsuser (
 );
 
 CREATE table task (
-    id serial primary key,
-    bsuser UUID references bsuser(id),
+    task_id serial primary key,
+    bsuser UUID references bsuser(user_id),
     task_name VARCHAR(99),
     description text,
     timestamp timestamp
