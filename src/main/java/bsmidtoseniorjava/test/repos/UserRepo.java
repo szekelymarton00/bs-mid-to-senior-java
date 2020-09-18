6d0ae9162ac8369c20e5ea53e21d3541c017674e
Marton Szekely <marton.szekely@blackswan.com>
2020-09-18 07:47:21 +0200

    dev: refactored userservices and repo

--- src/main/java/bsmidtoseniorjava/test/repos/UserRepo.java
+++ src/main/java/bsmidtoseniorjava/test/repos/UserRepo.java
@@ -5,23 +5,17 @@ import java.util.Optional;
 import java.util.UUID;
 
 import bsmidtoseniorjava.test.model.User;
+import org.springframework.data.jdbc.repository.query.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface UserRepo {
+@Repository("postgres")
+public interface UserRepo extends CrudRepository<User, UUID> {
 
-    int insertUser(UUID id, User user);
+    @Query("SELECT * FROM bsuser")
+    List<User> listAllUsers();
 
-    default int insertUser(User user) {
-        UUID id = UUID.randomUUID();
-        return insertUser(id, user);
-    }
-
-    List<User> selectAllUsers();
-
-    int deleteUserById(UUID id);
-
-    int updateUserById(UUID id, User user);
-
-    Optional<User> selectUserById(UUID id);
+    @Query("SELECT * FROM bsuser WHERE id = :id")
+    Optional<User> findById(@Param("id") UUID id);
 }
--- src/main/java/bsmidtoseniorjava/test/services/UserService.java
+++ src/main/java/bsmidtoseniorjava/test/services/UserService.java
@@ -1,42 +1,54 @@
 package bsmidtoseniorjava.test.services;
 
+import bsmidtoseniorjava.test.model.Task;
 import bsmidtoseniorjava.test.model.User;
 import bsmidtoseniorjava.test.repos.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 import java.util.UUID;
 
 @Service
 public class UserService {
 
     private final UserRepo userRepo;
+    private final JdbcTemplate jdbcTemplate;
 
     @Autowired
-    public UserService(@Qualifier("postgres") UserRepo userRepo) {
+    public UserService(@Qualifier("postgres") UserRepo userRepo, JdbcTemplate jdbcTemplate) {
         this.userRepo = userRepo;
+        this.jdbcTemplate = jdbcTemplate;
     }
 
-    public int addUser(User user) {
-        return userRepo.insertUser(user);
+    public void addUser(User user) {
+        UUID id = UUID.randomUUID();
+        jdbcTemplate.update("INSERT INTO bsuser(id, username, first_name, last_name, ownedtask) VALUES(?,?,?,?,?)", id, user.getUsername(),user.getFirstName(),user.getLastName(),user.getOwnedtask());
     }
 
-    public List<User> getAllUsers() {
-        return userRepo.selectAllUsers();
+    public Iterable<User> getAllUsers() {
+        return userRepo.listAllUsers();
     }
 
     public Optional<User> getUserById(UUID id) {
-        return userRepo.selectUserById(id);
+        return userRepo.findById(id);
     }
 
     public int deleteUser(UUID id) {
-        return userRepo.deleteUserById(id);
+        jdbcTemplate.update("DELETE FROM bsuser WHERE id = ?",id);
+        return 1;
     }
 
     public int updateUser(UUID id, User user) {
-        return userRepo.updateUserById(id,user);
+        jdbcTemplate.update("UPDATE bsuser SET username = ? WHERE id = ?",user.getUsername(),id);
+        return 1;
+    }
+
+    public Set<Task> getAllTasksOfUserById(UUID id) {
+       return userRepo.findById(id).get().getOwnedtask();
     }
 }
