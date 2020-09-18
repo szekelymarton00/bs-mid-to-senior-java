82e7329cfd506bc8a71cc5f2f131d4ec0b79f343
Marton Szekely <marton.szekely@blackswan.com>
2020-09-18 07:47:44 +0200

    dev: added annotations for postgres

--- src/main/java/bsmidtoseniorjava/test/model/User.java
+++ src/main/java/bsmidtoseniorjava/test/model/User.java
@@ -2,32 +2,45 @@ package bsmidtoseniorjava.test.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import javax.persistence.*;
+import java.util.Set;
 import java.util.UUID;
 
+@Entity
+@Table(name = "bsuser")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private final UUID id;
-    private final String name;
+    @Column(name="username")
+    private final String username;
+    @Column(name="first_name")
     private final String firstName;
+    @Column(name="last_name")
     private final String lastName;
+    @Column(name="ownedtask")
+    private final Set<Task> ownedtask;
 
 
     public User(@JsonProperty("id") UUID id,
-                @JsonProperty("username") String name,
+                @JsonProperty("username") String username,
                 @JsonProperty("first_name") String firstName,
-                @JsonProperty("last_name") String lastName) {
+                @JsonProperty("last_name") String lastName,
+                @JsonProperty("ownedtask") Set<Task> ownedtask) {
         this.id = id;
-        this.name = name;
+        this.username = username;
         this.firstName = firstName;
         this.lastName = lastName;
+        this.ownedtask = ownedtask;
     }
 
     public UUID getId() {
         return id;
     }
 
-    public String getName() {
-        return name;
+    public String getUsername() {
+        return username;
     }
 
     public String getFirstName() {
@@ -37,4 +50,9 @@ public class User {
     public String getLastName() {
         return lastName;
     }
+
+    public Set<Task> getOwnedtask() {
+        return ownedtask;
+    }
+
 }
