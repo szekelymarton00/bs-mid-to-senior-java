ab622275c6892e3da3f5eec9e8e69486f6f8f3bd
Marton Szekely <marton.szekely@blackswan.com>
2020-09-18 07:47:55 +0200

    dev: added table props

--- src/main/resources/db/migration/V1__UserTable.sql
+++ src/main/resources/db/migration/V1__UserTable.sql
@@ -1,6 +1,15 @@
 CREATE TABLE bsuser (
     id UUID NOT NULL PRIMARY KEY,
-    name VARCHAR(99) NOT NULL,
-    firstName VARCHAR(99),
-    lastName VARCHAR(99)
+    username VARCHAR(99) NOT NULL,
+    first_name VARCHAR(99),
+    last_name VARCHAR(99),
+    ownedtask VARCHAR(99)
 );
+
+CREATE table task (
+    id serial primary key,
+    bsuser UUID references bsuser(id),
+    task_name VARCHAR(99),
+    description text,
+    timestamp timestamp
+)
